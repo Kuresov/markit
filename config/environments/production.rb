41bdd62dde3a9717506f97133264e7f355efa10d
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-06-18 15:07:29 -0400

    Testing new settings for production Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # Default Mailer Host
+  config.action_mailer.default_url_options = {host: 'kuresov-markit.herokuapp.com'}
+  config.action_mailer.perform_deliveries = true
 end
