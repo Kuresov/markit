c7cfc2f21442f554ebfbe3f06cacca7cbf9b5e9f
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-06-18 21:30:03 -0400

    Devise, Figaro, and Mailgun setup

---
+++ config/initializers/mail.rb
@@ -0,0 +1,12 @@
+if Rails.env.development?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address:        'smtp.mailgun.org',
+    port:           '587',
+    authentication: :plain,
+    user_name:      ENV['MAILGUN_LOGIN'],
+    password:       ENV['MAILGUN_PASSWORD'],
+    domain:         'heroku.com',
+    enable_starttls_auto: true
+  }
+end
