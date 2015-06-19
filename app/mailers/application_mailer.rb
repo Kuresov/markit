a5ccf865ae28ea4102ab36e36404068c7f696ae5
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-06-18 23:24:51 -0400

    Switched ApplicationMailer default from address to Mailgun's provided

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "postmaster@app3efb071030fe4d83850e757afc71e9b1.mailgun.org"
   layout 'mailer'
 end
