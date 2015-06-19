abbd54cb18f86b5246f9ef8cdb5427af117b8a75
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-06-18 22:23:43 -0400

    Changing back the Mailgun URL

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,7 @@
 if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address:        'smtp.mailgun.com',
+    address:        'smtp.mailgun.org',
     port:           '587',
     authentication: :plain,
     user_name:      ENV['MAILGUN_LOGIN'],
