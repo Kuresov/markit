acee14ea14007637c8bf5d5c6044707f451fbf8c
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-06-18 22:18:15 -0400

    Added a mailer, changed Mailgun address as per their docs

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,7 @@
 if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address:        'smtp.mailgun.org',
+    address:        'smtp.mailgun.com',
     port:           '587',
     authentication: :plain,
     user_name:      ENV['MAILGUN_LOGIN'],
