715399671134cb8a5906acdc16517452d8ac3e4f
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-06-20 08:51:59 -0400

    Trying SSL port as per MailGun support's recco

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,7 +2,7 @@ if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:        'smtp.mailgun.org',
-    port:           587,
+    port:           465,
     authentication: :plain,
     user_name:      ENV['MAILGUN_LOGIN'],
     password:       ENV['MAILGUN_PASSWORD'],
