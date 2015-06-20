766578ede5a88a36d9fb1bd469aab3185a23a980
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-06-20 11:59:42 -0400

    Port should be a string, I think?

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,7 +2,7 @@ if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:        'smtp.mailgun.org',
-    port:           465,
+    port:           '587',
     authentication: :plain,
     user_name:      ENV['MAILGUN_SMTP_LOGIN'],
     password:       ENV['MAILGUN_SMTP_PASSWORD'],
