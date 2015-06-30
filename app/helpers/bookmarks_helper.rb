fc69330a493926b44b11badf19e35ace84ae895d
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-06-30 13:29:43 -0400

    Added text URL previews to bookmarks. Some validation being done by the model before the URL hits the DB. Need to investigate doing this on update as well -- should be simple

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,2 +1,12 @@
+require 'embedly'
+require 'json'
+
 module BookmarksHelper
+
+  def embed(url)
+    embedly_api = Embedly::API.new :key => ENV['EMBEDLY_API']
+    obj = embedly_api.oembed :url => url
+    return obj[0].marshal_dump
+  end
+
 end
